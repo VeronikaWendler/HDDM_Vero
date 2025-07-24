3b247f5182a6fb47d0b7e55cb6370e8d63017145
VeronikaWendler <wendler.vroni@gmail.com>
2025-07-25 01:02:16 +0200

    run

--- aDDM_Garcia_run_all_mod.py
+++ aDDM_Garcia_run_all_mod.py
@@ -287,6 +287,7 @@ def run_model(trace_id, data, model_dir, model_name, version, phase, samples=600
             t_reg = {'model': 't ~ 1 + C(OVcate)', 'link_func': lambda x: x}
             reg_descr = [v_reg, t_reg]
         elif version == 13:
+            z_link = make_z_link(data)
             v_reg = {'model': 'v ~ 1 + AttentionW + InattentionW', 'link_func': lambda x: x}
             z_reg = {'model': 'z ~ 1', 'link_func': z_link}
             reg_descr = [v_reg, z_reg]
