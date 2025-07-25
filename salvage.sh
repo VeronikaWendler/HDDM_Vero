3916f12df764527639152e91bb5f750183cfdbbc
VeronikaWendler <wendler.vroni@gmail.com>
2025-07-25 11:53:23 +0200

    aagain

--- salvage.sh
+++ salvage.sh
@@ -8,14 +8,17 @@
 #SBATCH --mail-type=ALL                     # email when job ends or fails
 #SBATCH --mail-user=u04vw21@abdn.ac.uk      # university email
 
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=u04vw21@abdn.ac.uk
+
 module load singularity/3.8.5
 
-IMAGE=$HOME/containers/hddm_latest.sif
-PROJECT=$HOME/sharedscratch/HDDM_Vero         # this has models_dir_garcia inside
-PREFIX=garcia_replication_ES_14               # ***basename only***
+IMAGE=$HOME/containers/hddm_latest.sif       # make sure it is readable
+PROJECT=$HOME/sharedscratch/HDDM_Vero        # contains models_dir_garcia
+PREFIX=garcia_replication_ES_14              # *basename only*
 
 singularity exec \
-  --bind ${PROJECT}:/workspace \
-  --pwd  /workspace/models_dir_garcia \       # <‑‑ set cwd inside container
-  ${IMAGE} \
-  python /workspace/salvage.py --auto ${PREFIX}
+  --bind "${PROJECT}:/workspace" \
+  --pwd  /workspace/models_dir_garcia \
+  "${IMAGE}" \
+  python /workspace/salvage.py --auto "${PREFIX}"
