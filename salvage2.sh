1ec9c565f69129d02f49648d12558ae334848061
VeronikaWendler <wendler.vroni@gmail.com>
2025-07-25 12:34:42 +0200

    again...

---
+++ salvage2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --partition=compute                 # CPU partition (on MacLeod, not sure about Maxwell)
+#SBATCH --cpus-per-task=8                   # number of CPU cores for chains
+#SBATCH --mem=200G                          # total memory for the job
+#SBATCH -o logs/salvage_%j.out
+#SBATCH -e logs/salvage_%j.err
+#SBATCH --mem=200G                          # total memory for the job
+#SBATCH --mail-type=ALL                     # email when job ends or fails
+#SBATCH --mail-user=u04vw21@abdn.ac.uk      # university email
+
+module load singularity/3.8.5
+
+IMAGE=$HOME/containers/hddm_latest.sif
+PROJECT=$HOME/sharedscratch/HDDM_Vero
+
+singularity exec \
+  --bind ${PROJECT}:/workspace \
+  ${IMAGE} \
+  bash -lc "cd /workspace/models_dir_garcia && ./salvage2.py"
