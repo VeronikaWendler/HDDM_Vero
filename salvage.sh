f079836ceaf127fe4a591ea4b675030ed47624b2
VeronikaWendler <wendler.vroni@gmail.com>
2025-07-25 11:02:40 +0200

    patching sh to save multiple chains

--- salvage.sh
+++ salvage.sh
@@ -8,11 +8,13 @@
 #SBATCH --mail-type=ALL                     # email when job ends or fails
 #SBATCH --mail-user=u04vw21@abdn.ac.uk      # university email
 
+
 module load singularity/3.8.5
 
 IMAGE=$HOME/containers/hddm_latest.sif
 PROJECT=$HOME/sharedscratch/HDDM_Vero
+export MPLCONFIGDIR=/workspace/.cache/mpl      # silence the warning
 
 singularity exec --bind ${PROJECT}:/workspace \
-        $IMAGE \
-        python /workspace/salvage.py
+                 $IMAGE \
+                 python /workspace/salvage.py --auto garcia_replication_ES_14
