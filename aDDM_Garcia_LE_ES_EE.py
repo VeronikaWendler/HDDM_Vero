d8ac0630ffff14011c0bc74ff4652a8120398231
VeronikaWendler <wendler.vroni@gmail.com>
2025-07-26 16:43:50 +0200

    plotting v16

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -69,7 +69,7 @@ from pathlib import Path
 # V_sub = value of the worse option
 
 # params:
-version = 15      # defining version
+version = 16      # defining version
 run = False        # if True, the the models run, if False the models load
 
 phase = ['ES']  #['ES', 'EE']  # Defines which phase you want ('ES', 'EE', 'LE', or the combinations)
