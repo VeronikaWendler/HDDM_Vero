c06d2f5b92dd566143f820c88fde0aa221703e2d
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-01 15:15:46 +0200

    ...

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -69,7 +69,7 @@ from pathlib import Path
 # V_sub = value of the worse option
 
 # params:
-version = 20    # defining version
+version = 21    # defining version
 run = False        # if True, the the models run, if False the models load
 
 phase = ['ES']  #['ES', 'EE']  # Defines which phase you want ('ES', 'EE', 'LE', or the combinations)
