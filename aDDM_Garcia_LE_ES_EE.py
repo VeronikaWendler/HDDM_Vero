e5da861608ba6dbd8b933e0d23d72d38f62295ec
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-07 21:57:09 +0200

    ...

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -2879,12 +2879,12 @@ def analyze_rl(infdatas, fig_dir, version):
             val = float(rhat[var].values)  # extract scalar
             f.write(f"{var}: {val:.3f}\n")
 
-    waic_res = az.waic(idata)
-    dic_df = pd.DataFrame({
-        "metric": ["DIC", "DIC_se"],
-        "value":  [waic_res.waic, waic_res.waic_se]
-    })
-    dic_df.to_csv(diag_dir/"dic.csv", index=False)
+    # waic_res = az.waic(idata)
+    # dic_df = pd.DataFrame({
+    #     "metric": ["DIC", "DIC_se"],
+    #     "value":  [waic_res.waic, waic_res.waic_se]
+    # })
+    # dic_df.to_csv(diag_dir/"dic.csv", index=False)
 
     # 4) Posterior predictive check
     #    if you have an RL PPC simulator you can plug it in here; otherwise skip
