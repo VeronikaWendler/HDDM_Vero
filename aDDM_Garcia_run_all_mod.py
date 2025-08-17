89af4812dbf13abde722ef2394428212f80fa16f
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-17 15:02:48 +0200

    ..

--- aDDM_Garcia_run_all_mod.py
+++ aDDM_Garcia_run_all_mod.py
@@ -531,7 +531,7 @@ def run_model(trace_id, data, model_dir, model_name, version, phase, samples=120
             reg_descr = [v_reg]
         elif version == 60:
             v_reg = {'model': 'v ~ 1 + AttentionW:C(OVcate) + IAW_chart + IAW_image', 'link_func': lambda x: x}
-            reg_descr = [v_reg, t_reg]
+            reg_descr = [v_reg]
             depends_on = {'a': 'OVcate'}
         elif version == 61:
             v_reg = {'model': 'v ~ 1 + AttentionW_E + AttentionW_S + InattentionW_E + InattentionW_S', 'link_func': lambda x: x}
