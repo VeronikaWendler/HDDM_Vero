cf29eeb041b96b72bd9a9eaebe4d0eaa947c7494
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-21 18:55:29 +0200

    ...

--- aDDM_Garcia_run_all_mod.py
+++ aDDM_Garcia_run_all_mod.py
@@ -163,7 +163,7 @@ RUN_ALL_MODELS  = True                                           # False = just 
 
 # selectivity
 start_phase = "ES_VAL"
-start_version = 6
+start_version = 7
 started = False
 
 # dir
