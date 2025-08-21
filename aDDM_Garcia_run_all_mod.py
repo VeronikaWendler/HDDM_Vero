a47c84b7b7e55874045c8f61eb664f65b679468a
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-21 14:15:55 +0200

    running ES_VAL 5

--- aDDM_Garcia_run_all_mod.py
+++ aDDM_Garcia_run_all_mod.py
@@ -142,7 +142,7 @@ model_versions  = {
                 "ES_ZBIAS_27"],
     "ES_quad": ["ES_quad_1","ES_quad_2"],
     "LE_RL": ["LE_RL_1","LE_RL_2"],
-    "ES_VAL": ["ES_VAL_1", "ES_VAL_2", "ES_VAL_3"]
+    "ES_VAL": ["ES_VAL_1", "ES_VAL_2", "ES_VAL_3", "ES_VAL_4", "ES_VAL_5", "ES_VAL_6"]
    
 }
 
