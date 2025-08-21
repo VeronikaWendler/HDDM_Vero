4ca0c8a75c953bd696f239bf9f14a0de6f8dbcec
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-21 18:13:49 +0200

    ..

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -71,7 +71,7 @@ numba.config.CACHE_ENABLE = False
 # ß1 = AttentionW,
 # ß2 = InattentionW,
 
-#
+##
 # ϵ = noise
 # PropDwell_opt = proportion of dwell time on the option with higher expected value
 # PropDwell_sub = proportion of dwell time on the option with lower expected value
