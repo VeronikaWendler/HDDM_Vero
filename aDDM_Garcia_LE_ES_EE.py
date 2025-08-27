fa5e120d538518d83155684e622768e2d72fbe5a
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-27 14:25:08 +0200

    ...

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -79,7 +79,7 @@ numba.config.CACHE_ENABLE = False
 # V_sub = value of the worse option
 
 # params:
-version = 25    # defining version #
+version = 26    # defining version #
 run = False        # if True, the the models run, if False the models load
 
 phase = ['ES_VAL']  #['ES', 'EE']  # Defines which phase you want ('ES', 'EE', 'LE', or the combinations)
