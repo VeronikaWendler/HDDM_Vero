e3d93462d1cf83b38d66eba21b3df9f1ebe7921f
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-31 00:53:01 +0200

    ...

--- aDDM_OV_run_all_mod.py
+++ aDDM_OV_run_all_mod.py
@@ -1507,6 +1507,8 @@ model_dir = BASE_MODEL_DIR
 if __name__ == "__main__":
 
     data_full = pd.read_csv((PROJECT_DIR / "data_sets" / "data_sets_OV" / "OVParticipants_Eye_Response_Feed_Allfix_addm_OV_Abs_CCT.csv").as_posix(), sep=",")
+    
+    
     # loop over phases and versions
     for phase in PHASE_RUN_ORDER:
         if phase in SKIP_PHASES:
