97197bb45c79a7071dd12e6b5d9fe0928e1d5fe9
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-31 00:31:04 +0200

    ...

--- run_hddm.sh
+++ run_hddm.sh
@@ -25,5 +25,5 @@ export MPLBACKEND=Agg
 singularity exec \
     --bind ${PROJECT}:/workspace \
     ${IMAGE} \
-    python /workspace/param_recovery_and_correlations/aDDM_OV_run_all_mod.py
+    python /workspace/aDDM_OV_run_all_mod.py
 
