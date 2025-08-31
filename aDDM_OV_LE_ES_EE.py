72fd8ca4b8a59264ec67e491bc434d62019a2b08
VeronikaWendler <wendler.vroni@gmail.com>
2025-08-31 13:50:03 +0200

    ...

--- aDDM_OV_LE_ES_EE.py
+++ aDDM_OV_LE_ES_EE.py
@@ -56,7 +56,7 @@ from pathlib import Path
 # v = β0 + β1 ⋅ (PropDwell_opt​ ⋅ V_opt​ − PropDwell_sub ⋅ V_sub) + β2,low ⋅ (PropDwell_sub ⋅ V_opt​ − PropDwell_opt​ ⋅ V_sub)+ β3 x (gazeS -gazeE)
 
 # params:
-version = 5     # set which version you want to run
+version = 6     # set which version you want to run
 run = False       # if True, the the models run, if False the models load
 
 phase = ['ES_VAL']  #['ES', 'EE']  #
