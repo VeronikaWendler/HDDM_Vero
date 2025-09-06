b430fde2b2eac1b053c2024e91c2e9662db41a25
VeronikaWendler <wendler.vroni@gmail.com>
2025-09-06 19:05:01 +0200

    plotting For_paper 6

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -79,7 +79,7 @@ numba.config.CACHE_ENABLE = False
 # V_sub = value of the worse option
 
 # params:
-version = 5    # defining version #
+version = 6    # defining version #
 run = False        # if True, the the models run, if False the models load
 
 phase = ['For_paper']  #['ES', 'EE']  # Defines which phase you want ('ES', 'EE', 'LE', or the combinations)
