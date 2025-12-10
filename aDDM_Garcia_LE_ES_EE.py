9cdcd84365d39eca2f9da1fb92cd83427acf6642
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-10 22:35:55 +0000

    ...

--- aDDM_Garcia_LE_ES_EE.py
+++ aDDM_Garcia_LE_ES_EE.py
@@ -2785,7 +2785,7 @@ def analyze_model(models, fig_dir, nr_models, version, phase):
             'Drift InattentionW',
             ]
             export_posterior_draws(
-                model_name="garcia_replication_For_paper_0",
+                model_name="garcia_replication_EE_0",
                 model_dir=BASE_MODEL_DIR,
                 n_jobs=nr_models,
                 S=1000
