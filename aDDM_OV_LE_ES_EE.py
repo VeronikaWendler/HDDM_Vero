b001776609ba355d6a3edd32aa926c7ab3c2f4de
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-11 16:19:57 +0000

    ....

--- aDDM_OV_LE_ES_EE.py
+++ aDDM_OV_LE_ES_EE.py
@@ -65,7 +65,7 @@ numba.config.CACHE_ENABLE = False
 # v = β0 + β1 ⋅ (PropDwell_opt​ ⋅ V_opt​ − PropDwell_sub ⋅ V_sub) + β2,low ⋅ (PropDwell_sub ⋅ V_opt​ − PropDwell_opt​ ⋅ V_sub)+ β3 x (gazeS -gazeE)
 
 # params:
-version = 8     # set which version you want to run
+version = 9     # set which version you want to run
 run = False       # if True, the the models run, if False the models load
 
 phase = ['For_paper']  #['ES', 'EE']  #
@@ -1999,7 +1999,7 @@ def analyze_model(models, fig_dir, nr_models, version, phase):
         #         'v_ES_AttentionW',
         #         'v_ES_InattentionW'
         #         ]   
-        elif version == 8:
+        elif version ==9:
             params_of_interest = [    
                 'a',
                 't',
