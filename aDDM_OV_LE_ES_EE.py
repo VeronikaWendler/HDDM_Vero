94e49e265def1f8cd716efe7a4b5cc5fbd9f1ea2
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-18 01:50:20 +0000

    plotting addm + z for OV

--- aDDM_OV_LE_ES_EE.py
+++ aDDM_OV_LE_ES_EE.py
@@ -65,7 +65,7 @@ numba.config.CACHE_ENABLE = False
 # v = β0 + β1 ⋅ (PropDwell_opt​ ⋅ V_opt​ − PropDwell_sub ⋅ V_sub) + β2,low ⋅ (PropDwell_sub ⋅ V_opt​ − PropDwell_opt​ ⋅ V_sub)+ β3 x (gazeS -gazeE)
 
 # params:
-version = 11     # set which version you want to run
+version = 2     # set which version you want to run
 run = False       # if True, the the models run, if False the models load
 
 phase = ['For_paper']  #['ES', 'EE']  #
@@ -1880,6 +1880,14 @@ def analyze_model(models, fig_dir, nr_models, version, phase):
                 'z',
                 'v_ES_AttentionW',
                 'v_ES_InattentionW']
+            
+            export_posterior_draws(
+                model_name="OV_replication_For_paper_2",
+                model_dir=BASE_MODEL_DIR,
+                n_jobs=nr_models,
+                S=1000)
+
+            
         elif version == 3:
             params_of_interest = [    
                 'a',
