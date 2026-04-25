45b828e640faf4dfe4a3264ae72b0503e416de04
VeronikaWendler <wendler.vroni@gmail.com>
2026-04-25 11:32:30 +0100

    ppc simulation

--- param_recovery_and_correlations/simulation_ppc.py
+++ param_recovery_and_correlations/simulation_ppc.py
@@ -26,12 +26,12 @@ warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 # ==========================================================
 
 model_paths = [
-    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_2_2.hddm",
-    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_2_1.hddm",
-    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_2_0.hddm"
+    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_3_2.hddm",
+    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_3_1.hddm",
+    "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/models/OV_replication_Final_3_0.hddm"
 ]
 
-output_dir = "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/figures/OV_replication_Final_2/ppc"
+output_dir = "/rds/projects/z/zhanglp-vwendler-core/HDDM_Vero/derivatives/hddm/figures/OV_replication_Final_3/ppc"
 os.makedirs(output_dir, exist_ok=True)
 
 analysis_name = "best_chain"
